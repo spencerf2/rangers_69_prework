c41fedb25617cfe045b38a1a30bbc8b424d3c70a
spencerf2 <spencer.franklin@hotmail.com>
2021-08-02 20:49:01 +0200

    first commit of prework

---
+++ prework.py
@@ -0,0 +1,11 @@
+# Question 1
+# ----------------------------------------------------------------------
+# Write a function to print "hello_USERNAME!" USERNAME is the input of 
+# the function. The first line of the code has been defined as below.
+# ----------------------------------------------------------------------
+# def hello_name(user_name):
+
+def hello_name(user_name):
+    print("hello_" + user_name.upper() + "!")
+
+hello_name("USERNAME")
